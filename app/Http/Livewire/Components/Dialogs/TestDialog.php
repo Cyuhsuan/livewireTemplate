65c9b94d0844146ee498ff8429bf7f85fdc259ef
yuHsuan <x89301089@gmail.com>
2021-10-15 07:24:12 +0800

    [add] 新增表單驗證

--- app/Http/Livewire/Components/Dialogs/TestDialog.php
+++ app/Http/Livewire/Components/Dialogs/TestDialog.php
@@ -6,8 +6,26 @@ use Livewire\Component;
 
 class TestDialog extends Component
 {
+    public $data;
+
+    protected $rules = [
+        'data.name' => 'required|min:6',
+        'data.email' => 'required|email',
+    ];
+    protected $messages = [
+        'data.name.required' => '姓名為必填',
+        'data.name.min' => '姓名為最少六個字',
+        'data.email.required' => 'email 為必填',
+        'data.email.email' => '必須為email格式',
+    ];
+
     public function render()
     {
         return view('livewire.components.dialogs.test-dialog')->layout('livewire.components.modal');
+    }
+
+    public function submit()
+    {
+        $this->validate();
     }
 }
--- resources/views/livewire/components/dialogs/test-dialog.blade.php
+++ resources/views/livewire/components/dialogs/test-dialog.blade.php
@@ -1,43 +1,27 @@
 <div>
-    <div id="dialog"
-        class="relative flex flex-col items-center justify-center pt-12 overflow-hidden bg-white h-80 w-80 rounded-2xl">
-        <div class="absolute top-0 left-0 flex items-center justify-end w-full h-12 px-8 bg-green-500"><button
-                wire:click="$emit('closeModal')">X</button></div>
-        <div class="overflow-y-auto">
-            <div class="p-5 text-center">
-                <h2 class="mb-10">Day17 常用範例：前後端共用的表單輸入驗證</h2>
-
-                <div class="flex justify-center mt-5">
-                    <div class="p-4 px-8 text-left border-2 rounded shadow-md bg-grey-50 w-96">
-                        <div class="my-5 ui form">
-                            <h4 class="mb-2 ui dividing header">請輸入用戶資料</h4>
-                            <div class="field">
-                                <label>姓名</label>
-                                <input type="text">
-                            </div>
-                            <div class="field">
-                                <label>手機號碼</label>
-                                <input type="text">
-                            </div>
-                            <div class="field">
-                                <label>Email</label>
-                                <input type="text">
-                            </div>
-                        </div>
-                        <div class="text-center">
-                            <button class="ui button">下一步</button>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-
+    <div class="absolute top-0 left-0 flex items-center justify-end w-full h-12 px-8 bg-green-500">
+        <button wire:click="$emit('closeModal')">X</button>
     </div>
-    <script>
-        document.getElementById('dialog').onclick = (e) => {
-            e.stopPropagation();
-        };
-    </script>
+    <div class="overflow-y-auto">
+        <div class="p-5 text-center">
+            <h2 class="mb-10">前後端共用的表單輸入驗證</h2>
+            <div class="flex flex-col justify-center mt-5">
+                <div class="flex flex-col mt-4">
+                    <label class="text-left">
+                        name
+                    </label>
+                    <input type="text" class="rounded-md" wire:model='data.name'>
+                    @error('data.name') <span class="text-red-500 error">*{{ $message }}</span> @enderror
+                </div>
+                <div class="flex flex-col mt-4">
+                    <label class="text-left">
+                        email
+                    </label>
+                    <input type="text" class="rounded-md" wire:model='data.email'>
+                    @error('data.email') <span class="text-red-500 error">*{{ $message }}</span> @enderror
+                </div>
+            </div>
+            <button wire:click="submit">送出</button>
+        </div>
+    </div>
 </div>
