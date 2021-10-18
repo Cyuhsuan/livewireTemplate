5be25038f86d2e00c411706dd48fc996091ba219
yuHsuan <x89301089@gmail.com>
2021-10-18 23:36:07 +0800

    [modify] header 新增 nav按鈕

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,16 +1,18 @@
 <div>
-    <div class="flex items-center justify-between h-12 px-8 bg-green-500">
+    <header class="flex items-center justify-between h-20 px-8 bg-gray-50">
         <h1>DEMO</h1>
-        @if (!$isLogin)
-            <button wire:click="$emit('openModal','login')">登入</button>
-        @else
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                            this.closest('form').submit();">
-                    <span>登出</span>
-                </a>
-            </form>
-        @endif
-    </div>
+        <nav>
+            <a href="" class="mr-8">HOME</a>
+            <a href="" class="mr-8">ABOUT US</a>
+            <a href="" class="mr-8">PRODUCTION</a>
+            @if (!$isLogin)
+                <button wire:click="$emit('openModal','login')">登入</button>
+            @else
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button>登出</button>
+                </form>
+            @endif
+        </nav>
+    </header>
 </div>
