6f6eded61b8dea8f7da8311ca5eec38d27a70299
yuHsuan <x89301089@gmail.com>
2021-10-18 23:31:38 +0800

    [add] introduction-card component

---
+++ app/Http/Livewire/Components/IntroductionCard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components;
+
+use Livewire\Component;
+
+class IntroductionCard extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.introduction-card');
+    }
+}
---
+++ resources/views/livewire/components/introduction-card.blade.php
@@ -0,0 +1,21 @@
+<div>
+    <div class="flex px-8 mt-4 h-44">
+        <div class="flex items-center justify-center w-2/4 border-2 border-solid h-90">
+            <div class="w-11/12 bg-gray-400 h-5/6"></div>
+        </div>
+        <div class="flex flex-col justify-between w-2/4 px-8">
+            <div>
+                <h2>this is title</h2>
+                <div>Aliquam mauris morbi tristique orci sit amet sapien tincidunt ut rutrum dui tincidunt.
+
+                    Cras eget lacinia magna nunc ut est est cras aliquam erat sem at dapibus lorem luctus sed nunc
+                    sagittis
+                    leo in…
+                </div>
+            </div>
+            <div class="flex justify-start">
+                <button>learn more</button>
+            </div>
+        </div>
+    </div>
+</div>
